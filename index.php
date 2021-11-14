8d88388e9970af340b17b8e998fd441037fb5ba2
MD Fahim Patowary <70106181+fahimgithub1@users.noreply.github.com>
2021-11-14 07:42:49 -1200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
+    //include_once("manhtml.html");
 
-include_once("manhtml.html");
+    $con = mysqli_connect('localhost', 'root', '','review');
+    
+    $txtMessage = $_POST['txtMessage'];
+
+    // database insert SQL code
+    $sql = "INSERT INTO `massages` (`comments`) VALUES ('$txtMessage')";
+
+    // insert in database 
+    $rs = mysqli_query($con, $sql);
+
+    if($rs)
+    {
+        echo "Contact Records Inserted";
+    }
 
 ?>
